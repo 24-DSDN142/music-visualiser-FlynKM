7ca4fc74a7f3ad4b04a1c934942353bfb3d7a405
FlynKM <kerrmlibe@myvuw.ac.nz>
2024-09-05 12:05:59 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -4,40 +4,64 @@ let last_words = "";
 let last_words_opacity = 0;
 
 function draw_one_frame(words, vocal, drum, bass, other,counter) {
-  background(255,236,180); // cream
-  fill(244,161,39); // orange
+  background(8,8,53); // dark blue
+  // fill(244,161,39); // orange
 
-  let stripeWidth = map(other, 40, 100, 40, 80, true);
+  // let stripeWidth = map(other, 50, 100, 40, 80, true);
 
-  let numStripes = height / stripeWidth;
-  for(let i=0; i<numStripes; i=i+2) {
-    let cury = map(i, 0, numStripes-1, 0, height);
-    rect(0, cury, width, stripeWidth);
-  }
+  // let numStripes = height / stripeWidth;
+  // for(let i=0; i<numStripes; i=i+2) {
+  //   let cury = map(i, 0, numStripes-1, 0, height);
+  //   rect(0, cury, width, stripeWidth);
+  // }
 
-  let triangleHeight = map(bass, 40, 100, 200, 550, true);
-  fill(117,200,174); // teal
-  for(let i=0; i<3; i++) {
-    let cur_x = map(i, 0, 4, 0, width);
-    let next_x = map(i+1, 0, 3, 0, width);
-    let mid_x = (cur_x + next_x) / 2.0;
-    //let cur_y = 4 * height / 5;
-    let cur_y = height 
-    triangle(cur_x, cur_y, mid_x, cur_y - triangleHeight, next_x, cur_y);
-  }
+  // let triangleHeight = map(bass, 40, 100, 200, 550, true);
+  // fill(117,200,174); // teal
+  // for(let i=0; i<3; i++) {
+  //   let cur_x = map(i, 0, 4, 0, width);
+  //   let next_x = map(i+1, 0, 3, 0, width);
+  //   let mid_x = (cur_x + next_x) / 2.0;
+  //   //let cur_y = 4 * height / 5;
+  //   let cur_y = height 
+  //   triangle(cur_x, cur_y, mid_x, cur_y - triangleHeight, next_x, cur_y);
+  // }
 
 
-  let drumSize = map(drum, 30, 100, 30, 300, true);
-  fill(90,61,43); // brown
-  rect(0, 0, drumSize, drumSize);
-  rect(width, 0, -drumSize, drumSize);
-  rect(0, height, drumSize, -drumSize);
-  rect(width, height, -drumSize, -drumSize);
+  let drumSize = map(drum, 30, 100, 200, 300, true);
+  // fill(90,61,43); // brown
+  // rect(0, 0, drumSize, drumSize);
+  // rect(width, 0, -drumSize, drumSize);
+  // rect(0, height, drumSize, -drumSize);
+  // rect(width, height, -drumSize, -drumSize);
 
-  let ovalPlace = map(vocal, 20, 100, height-50, 50, true);
-  let ovalSize = map(vocal, 20, 100, 60, 150, true);
-  fill(229,119,30); // darker orange
-  ellipse(width/2, ovalPlace, ovalSize);
+  strokeWeight(0)
+  fill(246,192,6) // yellow
+  ellipse(300,200,drumSize)
+ 
+  let starPoint = map(bass,30,70,90,10)
+
+  fill(500,500,500)
+  beginShape()
+    vertex(510,190-starPoint)//top point
+    vertex(500,220)
+    vertex(480-starPoint,230)//west point
+    vertex(500,240)
+    vertex(510,270+starPoint)//south 
+    vertex(520,240)
+    vertex(540+starPoint,230)//west point
+    vertex(520,220)
+   
+  endShape(CLOSE)
+
+
+  
+  
+
+
+  // let ovalPlace = map(vocal, 20, 100, height-50, 50, true);
+  // let ovalSize = map(vocal, 20, 100, 60, 150, true);
+  // fill(229,119,30); // darker orange
+  // ellipse(width/2, ovalPlace, ovalSize);
 
   if(words == "") {
     last_words_opacity = last_words_opacity * 0.95;
