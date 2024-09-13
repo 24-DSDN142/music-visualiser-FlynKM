d776e0c69d620aeb214be5de20e9eeb48f8a432c
FlynKM <kerrmlibe@myvuw.ac.nz>
2024-09-13 15:58:24 +1200

    Final details and refinement

--- music_starter.js
+++ music_starter.js
@@ -99,15 +99,6 @@ endShape(CLOSE)
   ellipse(700,300,5)
   ellipse(450,600,5)
 
-  
-  
-
-
-  // let ovalPlace = map(vocal, 20, 100, height-50, 50, true);
-  // let ovalSize = map(vocal, 20, 100, 60, 150, true);
-  // fill(229,119,30); // darker orange
-  // ellipse(width/2, ovalPlace, ovalSize);
-
 
 
   textFont('Georgia');
@@ -119,48 +110,4 @@ endShape(CLOSE)
   text(words, width/2, height/2);
 }
 
-    
   
-
-
-
-
-
-
-  
-
-
-  // let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
- 
-
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
- 
-  //  // display "words"
-  //  textAlign(CENTER);
-  //  textSize(vocal);
-  //  text(words, width/2, height/3);
